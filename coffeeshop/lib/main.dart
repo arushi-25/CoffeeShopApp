cdaec0046654d9df89516739b24588c58a8d27bd
Arushi Sharma <sharmaarushi045@gmail.com>
2024-09-20 00:07:33 +0530

    first commit

---
+++ coffeeshop/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:coffeeshop/screens/welcomescreen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: Color(0xFF212325)
+      ),
+      home: Welcomescreen(),
+    );
+  }
+}
---
+++ coffeeshop/lib/screens/welcomescreen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class Welcomescreen extends StatefulWidget {
+  const Welcomescreen({super.key});
+
+  @override
+  State<Welcomescreen> createState() => _WelcomescreenState();
+}
+
+class _WelcomescreenState extends State<Welcomescreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Container(
+        padding: EdgeInsets.only(top: 100, bottom: 40),
+        decoration: BoxDecoration(
+          color: Colors.black,
+          image: DecorationImage(
+            image: AssetImage("images/bp.png")
+          )
+        ),
+      ),
+    );
+  }
+}
